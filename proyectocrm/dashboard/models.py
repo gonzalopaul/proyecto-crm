ba9fde9ff7fff56091b9af186ec56001648e79f3
gonzalopaul <g.paul@usp.ceu.es>
2023-09-28 18:49:12 +0200

    Añadido el filtrado de categoría en backend

--- proyectocrm/dashboard/models.py
+++ proyectocrm/dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY = (
+('Crystal Box', 'Crystal Box'),
+('Crystal Bar', 'Crystal Bar'),
+('2800 puff', '2800 puff'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.quantity}'
