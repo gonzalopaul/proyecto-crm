39fc30fb5d640b9454d631629eee1b05bb894042
miriamjimenezj <miriam@ASUMiri>
2023-09-30 14:19:08 +0200

    Add order

--- proyectocrm/dashboard/models.py
+++ proyectocrm/dashboard/models.py
@@ -14,6 +14,9 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    class Meta:
+        verbose_name_plural = 'Product'
+
     def __str__(self):
         return f'{self.name} - {self.quantity}'
 
@@ -22,6 +25,9 @@ class Order(models.Model):
     staff = models.ForeignKey(User, models.CASCADE, null=True)
     order_quanity = models.PositiveIntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Order'
 
     def __str__(self):
         return f'{self.product} ordered by {self.staff.username}'
