7645f156e77a46e9a39be2c17e38a72c34838699
imartinpena <55758785+imartinpena@users.noreply.github.com>
2023-10-10 16:39:16 +0200

    Last update 10.10.2023

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from pathlib import Path
 
 
@@ -141,9 +142,5 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
