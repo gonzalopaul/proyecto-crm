822149c496418cba86f12795324965c469527201
imartinpena <55758785+imartinpena@users.noreply.github.com>
2023-10-10 16:17:03 +0200

    changes inactive reset password

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -117,12 +117,11 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'live.smtp.mailtrap.io'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'bd09e83a132004'
-EMAIL_HOST_PASSWORD = 'db64d41ca7bce9'
+EMAIL_HOST_USER = 'iglobalstore00@gmail.com'
+EMAIL_HOST_PASSWORD = 'Limpieza16'
 
 
 # Static files (CSS, JavaScript, Images)
